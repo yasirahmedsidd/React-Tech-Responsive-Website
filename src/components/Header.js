3dad418be6e4b88980fc0303979c2672bf7821ee
Yasir Ahmed Siddiqui <yasirahmedsidd@gmail.com>
2021-03-04 12:05:16 +0500

    mobile drawer menu added

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,13 @@
-import { Menu } from "antd";
-import { Anchor } from "antd";
+import { useState } from "react";
+import { Anchor, Drawer, Button } from "antd";
+import { BarsOutlined } from "@ant-design/icons";
 const { Link } = Anchor;
-
 const AppHeader = () => {
+  const [visible, setVisible] = useState(false);
+
+  const showDrawer = () => setVisible(true);
+  const onClose = () => setVisible(false);
+
   return (
     <div className="container-fluid">
       <div className="header">
@@ -10,15 +15,38 @@ const AppHeader = () => {
           <i className="fas fa-bolt "></i>
           <a href="https://google.com">Tech</a>
         </div>
-        <Anchor targetOffset={6}>
-          <Link href="#home" title="Home" />
-          <Link href="#about" title="About" />
-          <Link href="#features" title="Features" />
-          <Link href="#works" title="How it Works" />
-          <Link href="#faq" title="FAQ" />
-          <Link href="#pricing" title="Pricing" />
-          <Link href="#contact" title="Contact" />
-        </Anchor>
+        <div className="mobileHidden">
+          <Anchor targetOffset={65}>
+            <Link href="#home" title="Home" />
+            <Link href="#about" title="About" />
+            <Link href="#features" title="Features" />
+            <Link href="#works" title="How it Works" />
+            <Link href="#faq" title="FAQ" />
+            <Link href="#pricing" title="Pricing" />
+            <Link href="#contact" title="Contact" />
+          </Anchor>
+        </div>
+        <div className="mobileVisible">
+          <Button type="primary" onClick={showDrawer}>
+            <i className="fas fa-bars"></i>
+          </Button>
+          <Drawer
+            placement="right"
+            closable={false}
+            onClose={onClose}
+            visible={visible}
+          >
+            <Anchor targetOffset={65}>
+              <Link href="#home" title="Home" />
+              <Link href="#about" title="About" />
+              <Link href="#features" title="Features" />
+              <Link href="#works" title="How it Works" />
+              <Link href="#faq" title="FAQ" />
+              <Link href="#pricing" title="Pricing" />
+              <Link href="#contact" title="Contact" />
+            </Anchor>
+          </Drawer>
+        </div>
       </div>
     </div>
   );
